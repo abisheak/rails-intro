0483f933273f3c63f64b78bdcd1207698bf760a9
usa.powernet@gmail.com <usa.powernet@gmail.com>
2014-11-24 08:20:46 +0530

    Adding links

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,9 @@ Rottenpotatoes::Application.routes.draw do
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :movies
-
+  resources :movies do
+    get "sort"
+  end
   # Sample resource route with options:
   #   resources :products do
   #     member do
